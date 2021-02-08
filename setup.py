7b6ffd380e3f7c05c6ada0d5ef0b87c4f3b90be1
Gabriel Ciolac <gabrielciolac@gmail.com>
2021-02-08 17:11:54 -0500

    update to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['your_package'],
+    packages=['roomba'],
     package_dir={'': 'scripts'})
 
 setup(**setup_args)
