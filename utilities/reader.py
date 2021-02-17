2bdd167b29b951562696cc1a2c50bcdf7a7592ca
gabrielciolac <gabrielciolac@gmail.com>
2021-02-17 14:18:11 -0500

    Reader utility

---
+++ utilities/reader.py
@@ -0,0 +1,17 @@
+import os
+    __path = os.path.dirname(__file__)
+    __path = os.path.relpath('..\\resources\\beacons',__path)
+    
+    '''
+        A utility to read from the beacon file
+    '''
+    def read_beacons():
+        with open(__path,'r') as f:
+            beacons = f.read()
+            f.close()
+        macs = dict()
+        lines = beacons.splitlines()
+        for l in lines:
+            csv = l.split(',')
+            macs[csv[1]] = (csv[2],csv[3])
+
